e0e079e113f8de3763823fc986eca2e45931e606
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 19:10:53 +0300

    fix: add lineage event package wrapper

---
+++ opticell/lineage_events.py
@@ -0,0 +1,4 @@
+"""Public wrapper for lineage event diagnostics."""
+from lineage_events import division_consistency, lineage_event_summary
+
+__all__ = ["division_consistency", "lineage_event_summary"]
