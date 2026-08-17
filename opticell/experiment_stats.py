4cf5194000624e2701f42ba0b98b95d9b26e18c2
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:57:07 +0300

    feat: expose experiment-level statistics under public opticell namespace

---
+++ opticell/experiment_stats.py
@@ -0,0 +1,4 @@
+"""Public high-level experiment statistics API."""
+from experiment_stats import bootstrap_ci, replicate_effect_summary, summarize_experiment
+
+__all__ = ["bootstrap_ci", "replicate_effect_summary", "summarize_experiment"]
