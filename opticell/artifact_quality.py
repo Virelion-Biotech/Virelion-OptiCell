07e4764b82009d8ec42e5c4321c373177572ea8c
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 22:13:39 +0300

    feat: expose acquisition artifact metrics

---
+++ opticell/artifact_quality.py
@@ -0,0 +1,4 @@
+"""Public acquisition-artifact QC API."""
+from artifact_quality import acquisition_artifact_metrics, artifact_burden_score
+
+__all__ = ["acquisition_artifact_metrics", "artifact_burden_score"]
