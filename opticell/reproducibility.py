5882c0074f5ddea5455e0ce0c1edb3a7c4d4f172
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 01:10:35 +0300

    Expose reproducibility API

---
+++ opticell/reproducibility.py
@@ -0,0 +1,4 @@
+"""Package wrapper for reproducibility helpers."""
+from reproducibility import analysis_fingerprint, canonical_json, compare_manifests, environment_fingerprint
+
+__all__ = ["analysis_fingerprint", "canonical_json", "compare_manifests", "environment_fingerprint"]
