d1338f6049b163af694914baa2052f772290cecc
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:45:56 +0300

    fix: expose reporting utilities in opticell namespace

---
+++ opticell/reporting.py
@@ -0,0 +1,4 @@
+"""Public namespace for OptiCell reporting utilities."""
+from reporting import RuntimeStats, build_report, dataframe_summary, runtime_stats, write_report
+
+__all__ = ["RuntimeStats", "runtime_stats", "dataframe_summary", "build_report", "write_report"]
