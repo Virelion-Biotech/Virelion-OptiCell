a3ed120e697a8f40ae65e7d7701d14c6298132aa
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:56:52 +0300

    feat: expose benchmarking under public opticell namespace

---
+++ opticell/benchmarking.py
@@ -0,0 +1,4 @@
+"""Public segmentation benchmarking API."""
+from benchmarking import aggregate_backend_benchmarks, benchmark_backends
+
+__all__ = ["benchmark_backends", "aggregate_backend_benchmarks"]
