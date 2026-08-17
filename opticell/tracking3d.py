50248a16328c437e9f84357cc16de9e19fdc6085
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:45:45 +0300

    fix: expose 3-D tracker through opticell namespace

---
+++ opticell/tracking3d.py
@@ -0,0 +1,4 @@
+"""Public namespace for 3-D time-lapse tracking."""
+from tracking3d import Tracking3DConfig, link_frames_3d, summarize_tracks_3d
+
+__all__ = ["Tracking3DConfig", "link_frames_3d", "summarize_tracks_3d"]
