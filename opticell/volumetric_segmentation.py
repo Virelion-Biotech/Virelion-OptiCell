6310e83ae9f3872bbc5f2a7291fb3ac54852b23b
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:33:59 +0300

    feat: expose public 3-D segmentation namespace

---
+++ opticell/volumetric_segmentation.py
@@ -0,0 +1,4 @@
+"""Public namespace for deterministic 3-D segmentation."""
+from volumetric_segmentation import VolumetricSegmentationResult, segment_threshold_3d
+
+__all__ = ["VolumetricSegmentationResult", "segment_threshold_3d"]
