9adc39be9092b186bb1e0024f8e53b0ce88e1a28
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 22:14:20 +0300

    feat: expose OptiCell 2.14 quality APIs

--- opticell/__init__.py
+++ opticell/__init__.py
@@ -3,6 +3,8 @@
 from qc_pipeline import QCThresholds, analyze_folder, analyze_image, analyze_paths, extract_object_features
 from quantitative import add_spatial_features, object_channel_intensity, summarize_spatial_features
 from validation import benchmark_segmentation, paired_segmentation_metrics
+from .acceptance import SegmentationAcceptance, segmentation_acceptance
+from .artifact_quality import acquisition_artifact_metrics, artifact_burden_score
 from .batch import BatchConfig, analyze_paths_parallel
 from .benchmarking import aggregate_backend_benchmarks, benchmark_backends
 from .exports import dataframe_to_long_form, write_dataframe
@@ -16,6 +18,7 @@ from .power import two_group_sample_size
 from .provenance import build_manifest, collect_input_manifest, file_sha256, write_manifest
 from .profiling import ProfileRecord, profile_call, profile_records, summarize_profile
 from .reporting import RuntimeStats, build_report, dataframe_summary, runtime_stats, write_report
+from .robustness import stable_parameter_subset, summarize_sensitivity
 from .runtime import RuntimeCapabilities, capabilities, capabilities_dict, measure, preferred_accelerator
 from .screening import percent_control, z_prime_factor
 from .screening_qc import AssayQCDecision, assay_qc_decision, classify_z_prime
@@ -29,18 +32,17 @@ from .volumetric import nearest_neighbor_distances_3d, summarize_volume, volume_
 from .volumetric_segmentation import VolumetricSegmentationResult, segment_threshold_3d
 
 __all__ = [
-    "AssayQCDecision", "BaseSegmenter", "BatchConfig", "CellposeBackend", "OMEImageInfo", "ProfileRecord", "QCThresholds", "RuntimeCapabilities", "RuntimeStats",
-    "ThresholdSegmenter", "Tracking3DConfig", "VolumetricSegmentationResult", "add_spatial_features", "aggregate_backend_benchmarks",
-    "analyze_folder", "analyze_image", "analyze_paths", "analyze_paths_parallel", "assay_qc_decision", "available_backends", "benchmark_backends",
-    "benchmark_segmentation", "benjamini_hochberg", "bootstrap_ci", "build_lineage_table", "build_manifest", "build_report",
-    "capabilities", "capabilities_dict", "classify_divisions", "classify_z_prime", "collect_input_manifest", "compare_backends", "compare_two_groups",
-    "dataframe_summary", "dataframe_to_long_form", "detect_time_series_events", "detect_transition_events", "division_consistency", "extract_object_features",
-    "file_sha256", "get_backend", "iter_array_chunks", "iter_tiff_frames", "lineage_event_summary", "lineage_quality_summary", "link_frames_3d", "load_ome_series",
-    "measure", "memmap_tiff", "nearest_neighbor_distances_3d", "normalize_to_controls", "object_channel_intensity", "paired_segmentation_metrics",
-    "percent_control", "plate_edge_effect", "plate_qc_summary", "preferred_accelerator", "profile_call", "profile_records", "read_ome_info",
-    "register_backend", "replicate_effect_summary", "robust_zscore", "runtime_stats", "segment_threshold_3d", "summarize_by_replicate",
-    "summarize_experiment", "summarize_lineages", "summarize_profile", "summarize_spatial_features", "summarize_tracks_3d", "summarize_volume",
+    "AssayQCDecision", "BaseSegmenter", "BatchConfig", "CellposeBackend", "OMEImageInfo", "ProfileRecord", "QCThresholds", "RuntimeCapabilities", "RuntimeStats", "SegmentationAcceptance",
+    "ThresholdSegmenter", "Tracking3DConfig", "VolumetricSegmentationResult", "acquisition_artifact_metrics", "add_spatial_features", "aggregate_backend_benchmarks", "analyze_folder",
+    "analyze_image", "analyze_paths", "analyze_paths_parallel", "artifact_burden_score", "assay_qc_decision", "available_backends", "benchmark_backends", "benchmark_segmentation",
+    "benjamini_hochberg", "bootstrap_ci", "build_lineage_table", "build_manifest", "build_report", "capabilities", "capabilities_dict", "classify_divisions", "classify_z_prime",
+    "collect_input_manifest", "compare_backends", "compare_two_groups", "dataframe_summary", "dataframe_to_long_form", "detect_time_series_events", "detect_transition_events",
+    "division_consistency", "extract_object_features", "file_sha256", "get_backend", "iter_array_chunks", "iter_tiff_frames", "lineage_event_summary", "lineage_quality_summary",
+    "link_frames_3d", "load_ome_series", "measure", "memmap_tiff", "nearest_neighbor_distances_3d", "normalize_to_controls", "object_channel_intensity", "paired_segmentation_metrics",
+    "percent_control", "plate_edge_effect", "plate_qc_summary", "preferred_accelerator", "profile_call", "profile_records", "read_ome_info", "register_backend",
+    "replicate_effect_summary", "robust_zscore", "runtime_stats", "segment_threshold_3d", "segmentation_acceptance", "stable_parameter_subset", "summarize_by_replicate",
+    "summarize_experiment", "summarize_lineages", "summarize_profile", "summarize_sensitivity", "summarize_spatial_features", "summarize_tracks_3d", "summarize_volume",
     "threshold_sensitivity", "two_group_sample_size", "volume_features", "write_dataframe", "write_manifest", "write_report", "z_prime_factor",
 ]
 
-__version__ = "2.13.0"
+__version__ = "2.14.0"
