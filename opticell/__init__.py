648bfbdc84ce3a873e08a0c3ba7697f96b2573a8
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 18:01:38 +0300

    api: expose OptiCell 2.10 public API

--- opticell/__init__.py
+++ opticell/__init__.py
@@ -4,33 +4,81 @@ from qc_pipeline import QCThresholds, analyze_folder, analyze_image, analyze_pat
 from quantitative import add_spatial_features, object_channel_intensity, summarize_spatial_features
 from validation import benchmark_segmentation, paired_segmentation_metrics
 from .batch import BatchConfig, analyze_paths_parallel
-from .statistics import benjamini_hochberg, compare_two_groups, summarize_by_replicate
-from .volumetric import nearest_neighbor_distances_3d, summarize_volume, volume_features
-from .segmentation import BaseSegmenter, CellposeBackend, ThresholdSegmenter, available_backends, compare_backends, get_backend, register_backend
-from .volumetric_segmentation import VolumetricSegmentationResult, segment_threshold_3d
-from .tracking3d import Tracking3DConfig, link_frames_3d, summarize_tracks_3d
-from .provenance import build_manifest, collect_input_manifest, file_sha256, write_manifest
-from .reporting import RuntimeStats, build_report, dataframe_summary, runtime_stats, write_report
 from .benchmarking import aggregate_backend_benchmarks, benchmark_backends
-from .runtime import RuntimeCapabilities, capabilities, capabilities_dict, measure, preferred_accelerator
-from .tracking_events import classify_divisions, detect_time_series_events, detect_transition_events
+from .experiment_qc import normalize_to_controls, plate_edge_effect, plate_qc_summary, robust_zscore
 from .experiment_stats import bootstrap_ci, replicate_effect_summary, summarize_experiment
+from .provenance import build_manifest, collect_input_manifest, file_sha256, write_manifest
+from .profiling import ProfileRecord, profile_call, profile_records, summarize_profile
+from .reporting import RuntimeStats, build_report, dataframe_summary, runtime_stats, write_report
+from .runtime import RuntimeCapabilities, capabilities, capabilities_dict, measure, preferred_accelerator
+from .segmentation import BaseSegmenter, CellposeBackend, ThresholdSegmenter, available_backends, compare_backends, get_backend, register_backend
+from .statistics import benjamini_hochberg, compare_two_groups, summarize_by_replicate
+from .tracking3d import Tracking3DConfig, link_frames_3d, summarize_tracks_3d
+from .tracking_events import classify_divisions, detect_time_series_events, detect_transition_events
+from .volumetric import nearest_neighbor_distances_3d, summarize_volume, volume_features
+from .volumetric_segmentation import VolumetricSegmentationResult, segment_threshold_3d
 
 __all__ = [
-    "QCThresholds", "analyze_folder", "analyze_image", "analyze_paths", "extract_object_features",
-    "add_spatial_features", "object_channel_intensity", "summarize_spatial_features",
-    "benchmark_segmentation", "paired_segmentation_metrics",
-    "BatchConfig", "analyze_paths_parallel", "summarize_by_replicate", "compare_two_groups", "benjamini_hochberg",
-    "volume_features", "summarize_volume", "nearest_neighbor_distances_3d",
-    "BaseSegmenter", "ThresholdSegmenter", "CellposeBackend", "register_backend", "available_backends", "get_backend", "compare_backends",
-    "VolumetricSegmentationResult", "segment_threshold_3d",
-    "Tracking3DConfig", "link_frames_3d", "summarize_tracks_3d",
-    "file_sha256", "collect_input_manifest", "build_manifest", "write_manifest",
-    "RuntimeStats", "runtime_stats", "dataframe_summary", "build_report", "write_report",
-    "benchmark_backends", "aggregate_backend_benchmarks",
-    "RuntimeCapabilities", "capabilities", "capabilities_dict", "preferred_accelerator", "measure",
-    "detect_transition_events", "detect_time_series_events", "classify_divisions",
-    "bootstrap_ci", "replicate_effect_summary", "summarize_experiment",
+    "BaseSegmenter",
+    "BatchConfig",
+    "CellposeBackend",
+    "ProfileRecord",
+    "QCThresholds",
+    "RuntimeCapabilities",
+    "RuntimeStats",
+    "ThresholdSegmenter",
+    "Tracking3DConfig",
+    "VolumetricSegmentationResult",
+    "add_spatial_features",
+    "aggregate_backend_benchmarks",
+    "analyze_folder",
+    "analyze_image",
+    "analyze_paths",
+    "analyze_paths_parallel",
+    "available_backends",
+    "benchmark_backends",
+    "benchmark_segmentation",
+    "benjamini_hochberg",
+    "bootstrap_ci",
+    "build_manifest",
+    "build_report",
+    "capabilities",
+    "capabilities_dict",
+    "classify_divisions",
+    "collect_input_manifest",
+    "compare_backends",
+    "compare_two_groups",
+    "dataframe_summary",
+    "detect_time_series_events",
+    "detect_transition_events",
+    "extract_object_features",
+    "file_sha256",
+    "get_backend",
+    "link_frames_3d",
+    "measure",
+    "nearest_neighbor_distances_3d",
+    "normalize_to_controls",
+    "object_channel_intensity",
+    "paired_segmentation_metrics",
+    "plate_edge_effect",
+    "plate_qc_summary",
+    "preferred_accelerator",
+    "profile_call",
+    "profile_records",
+    "register_backend",
+    "replicate_effect_summary",
+    "robust_zscore",
+    "runtime_stats",
+    "segment_threshold_3d",
+    "summarize_by_replicate",
+    "summarize_experiment",
+    "summarize_profile",
+    "summarize_spatial_features",
+    "summarize_tracks_3d",
+    "summarize_volume",
+    "volume_features",
+    "write_manifest",
+    "write_report",
 ]
 
-__version__ = "2.9.0"
+__version__ = "2.10.0"
