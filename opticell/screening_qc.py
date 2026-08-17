1430d2403cf07be2127adac2003110743e2dab1d
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 19:11:06 +0300

    fix: add screening QC package wrapper

---
+++ opticell/screening_qc.py
@@ -0,0 +1,4 @@
+"""Public wrapper for screening assay QC decisions."""
+from screening_qc import AssayQCDecision, assay_qc_decision, classify_z_prime
+
+__all__ = ["AssayQCDecision", "assay_qc_decision", "classify_z_prime"]
