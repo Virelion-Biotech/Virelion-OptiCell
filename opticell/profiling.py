4f4cadd00aa901c77904d17cb39212948553214f
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 18:01:25 +0300

    api: expose profiling module

---
+++ opticell/profiling.py
@@ -0,0 +1,4 @@
+"""Stable package namespace for OptiCell profiling."""
+from profiling import ProfileRecord, profile_call, profile_records, summarize_profile
+
+__all__ = ["ProfileRecord", "profile_call", "profile_records", "summarize_profile"]
