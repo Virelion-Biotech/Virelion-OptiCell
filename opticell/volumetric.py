b69338d7b2bf7e8a0494043cd8b5c0124736c8a4
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:28:45 +0300

    feat: expose volumetric analysis through public package API

---
+++ opticell/volumetric.py
@@ -0,0 +1,4 @@
+"""Public namespace for 3-D volumetric microscopy analysis."""
+from volumetric import nearest_neighbor_distances_3d, summarize_volume, volume_features
+
+__all__ = ["nearest_neighbor_distances_3d", "summarize_volume", "volume_features"]
