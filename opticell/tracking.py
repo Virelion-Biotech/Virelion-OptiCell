8f32205c3571ebce7e44eefd43e6473547d8b5c1
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 14:19:00 +0300

    refactor: expose tracking through opticell package

---
+++ opticell/tracking.py
@@ -0,0 +1,4 @@
+"""Stable package namespace for time-lapse tracking."""
+from tracking import TrackingConfig, link_frames, summarize_tracks
+
+__all__ = ["TrackingConfig", "link_frames", "summarize_tracks"]
